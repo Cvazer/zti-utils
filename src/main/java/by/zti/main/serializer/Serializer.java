a19a3497e9c83a338a4994045dc70d7d25723321
Cvazer <zhirnitoni@gmail.com>
2017-04-17 19:51:28 +0300

    docs update

--- src/main/java/by/zti/main/serializer/Serializer.java
+++ src/main/java/by/zti/main/serializer/Serializer.java
@@ -2,13 +2,28 @@ package by.zti.main.serializer;
 
 import java.io.*;
 
+/**
+ * This utility class works from an instance and allows you to easily serialize and deserialize objects from your program.
+ * @param <T> Class type for instance to work with.
+ *
+ * @author Yan Frankovski
+ * @since ZTIU 1.0.2
+ */
 public class Serializer<T> {
+    /** This value stores reference to file to work with */
     private File file;
 
+    /**
+     * Empty constructor for creating empty instance
+     */
     public Serializer() {
         this("NULL");
     }
 
+    /**
+     * Constructor that creates an instance of {@code Serializer} class and associates it with file by given path.
+     * @param path  {@code String} path to the file.
+     */
     public Serializer(String path) {
         file = new File(path);
         if(!file.exists()){
@@ -20,10 +35,18 @@ public class Serializer<T> {
         }
     }
 
+    /**
+     * Constructor that creates an instance of {@code Serializer} class and associates it with given {@code File}.
+     * @param file A file to work with.
+     */
     public Serializer(File file) {
         this.file = file;
     }
 
+    /**
+     * This method should be called to serialize object to stored {@code File} location.
+     * @param obj {@code Object} to be serialized.
+     */
     public void serialize(T obj){
         try {
             FileOutputStream fis = new FileOutputStream(file);
@@ -36,6 +59,10 @@ public class Serializer<T> {
         }
     }
 
+    /**
+     * This method should be called to deserialize object from stored {@code File} location.
+     * @return {@code Object} deserialized from stored {@code File} location.
+     */
     public T deserialize(){
         try {
             FileInputStream fis = new FileInputStream(file);
@@ -47,10 +74,16 @@ public class Serializer<T> {
         return null;
     }
 
+    /**
+     * @return returns stored {@code File} instance.
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * @param file New {@code File} instance to be stored.
+     */
     public void setFile(File file) {
         this.file = file;
     }
